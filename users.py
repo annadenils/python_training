5101b40a4d458c79d6b41403e71428ce78b0b895
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2020-12-11 22:25:33 +0300

    Задание №3: Сделать всё то же самое, но для контактов

--- users.py
+++ users.py
@@ -1,14 +1,17 @@
 class Users():
-    def __init__(self, user_lastname, user_middlename, user_name, address_company, name_company, fax, work_phone, mobile_phone, home_phone, byear, bmonth, bday):
-        self.user_lastname = user_lastname
-        self.user_middlename = user_middlename
-        self.user_name = user_name
+    def __init__(self, users_lastname, users_middlename, users_name, nickname, address_company, name_company, fax, work_phone, mobile_phone, home_phone, email, byear, bmonth, bday, address2):
+        self.users_lastname = users_lastname
+        self.users_middlename = users_middlename
+        self.users_name = users_name
+        self.nickname = nickname
         self.address_company = address_company
         self.name_company = name_company
         self.fax = fax
         self.work_phone = work_phone
         self.mobile_phone = mobile_phone
         self.home_phone = home_phone
+        self.email = email
         self.byear = byear
         self.bmonth = bmonth
         self.bday = bday
+        self.address2 = address2
