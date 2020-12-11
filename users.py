0bc038d5c880b5a07a0927d77a8f495139972551
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2020-12-11 21:38:41 +0300

    не работает

---
+++ users.py
@@ -0,0 +1,14 @@
+class Users():
+    def __init__(self, user_lastname, user_middlename, user_name, address_company, name_company, fax, work_phone, mobile_phone, home_phone, byear, bmonth, bday):
+        self.user_lastname = user_lastname
+        self.user_middlename = user_middlename
+        self.user_name = user_name
+        self.address_company = address_company
+        self.name_company = name_company
+        self.fax = fax
+        self.work_phone = work_phone
+        self.mobile_phone = mobile_phone
+        self.home_phone = home_phone
+        self.byear = byear
+        self.bmonth = bmonth
+        self.bday = bday
