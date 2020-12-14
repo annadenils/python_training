12eb2a8e1f2d8824700462eda1c593fedbb3ea57
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2020-12-14 20:53:26 +0300

    оптимизация

--- test_add_group.py
+++ test_add_group.py
@@ -18,6 +18,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://127.0.0.1/addressbook/index.php")
 
     def login(self, wd, user_name, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(user_name)
         wd.find_element_by_name("pass").click()
@@ -29,6 +30,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def init_group(self, wd, group):
+        self.open_groups_page(wd)
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -40,6 +42,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.comment)
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
@@ -49,20 +52,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
-        self.open_groups_page(wd)
         self.init_group(wd, Group(group_name="new_group", header="logo", comment="comment"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, user_name="admin", password="secret")
-        self.open_groups_page(wd)
         self.init_group(wd, Group(group_name="", header="", comment=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def is_element_present(self, how, what):
