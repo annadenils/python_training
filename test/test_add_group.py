0a36165bce95d3cc9d5f16b7bc8d4fe1d2e50dec
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2020-12-15 23:50:34 +0300

    class group for group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,10 +11,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(login_name="admin", password="secret")
-    app.init_group(Group(group_name="new_group", header="logo", comment="comment"))
+    app.group.init_group(Group(group_name="new_group", header="logo", comment="comment"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(login_name="admin", password="secret")
-    app.init_group(Group(group_name="", header="", comment=""))
+    app.group.init_group(Group(group_name="", header="", comment=""))
     app.session.logout()
