b227a1671fcf5caa2586d3eadd57acdea760f3ae
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2020-12-15 22:42:43 +0300

    выделен класс session

--- fixture/application_group.py
+++ fixture/application_group.py
@@ -1,24 +1,16 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 class Application_group():
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://127.0.0.1/addressbook/index.php")
-
-    def login(self, user_name, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user_name)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -43,10 +35,6 @@ class Application_group():
     def return_to_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,11 +10,11 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(user_name="admin", password="secret")
+    app.session.login(user_name="admin", password="secret")
     app.init_group(Group(group_name="new_group", header="logo", comment="comment"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(user_name="admin", password="secret")
+    app.session.login(user_name="admin", password="secret")
     app.init_group(Group(group_name="", header="", comment=""))
-    app.logout()
+    app.session.logout()
