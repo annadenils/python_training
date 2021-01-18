bf4e759de018000265d3de88f4f0d983e837c574
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2021-01-18 20:54:11 +0300

    добавление выбора случайного элемента в группах

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,11 +1,13 @@
 from model.group import Group
+from random import randrange
 
 def test_del_group(app):
     if app.group.count() == 0:
         app.group.init_group(Group(group_name="test-test"))
     old_groups = app.group.get_group_list()
-    app.group.del_group()
+    index = randrange(len(old_groups))
+    app.group.del_group_by_index(index)
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
-    old_groups[0:1] = []
+    old_groups[index:index+1] = []
     assert old_groups == new_groups
