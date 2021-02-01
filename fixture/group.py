b8704b9ee88b40c2671a661ba6c3caf39dfa918c
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2021-02-01 22:42:14 +0300

    modify_group из db

--- fixture/group.py
+++ fixture/group.py
@@ -69,7 +69,17 @@ class GroupHelper:
     def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_group_by_index(index)
+        self.select_group_by_id(index)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def modify_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,15 +1,14 @@
 from model.group import Group
-from random import randrange
+import random
 
-def test_modify_group(app):
-    if app.group.count() == 0:
+def test_modify_group(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
         app.group.init_group(Group(group_name="test-test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(group_name="modify_group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
-    new_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    group1 = Group(group_name="modify_group")
+    app.group.modify_group_by_id(group.id, group1)
+    new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
