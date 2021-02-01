c5983e9c4f723807f03b2f72a54c4318f66caf0c
Anna Kosolapova <anna.kosolapova@homecredit.ru>
2021-02-01 21:06:36 +0300

    orm-отношения

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,9 +1,10 @@
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_user_list()
+    l = db.get_users_not_in_group(Group(id="2"))
     for item in l:
         print(item)
     print(len(l))
--- fixture/orm.py
+++ fixture/orm.py
@@ -13,6 +13,7 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        users = Set(lambda: ORMFixture.ORMUsers, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
     class ORMUsers(db.Entity):
         _table_ = 'addressbook'
@@ -20,6 +21,7 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="users", lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password)
@@ -43,3 +45,12 @@ class ORMFixture:
             return Users(id=str(user.id), users_name=user.firstname, users_lastname=user.lastname)
         return list(map(convert, users))
 
+    @db_session
+    def get_users_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_users_to_model(orm_group.users)
+
+    @db_session
+    def get_users_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_users_to_model(select(c for c in ORMFixture.ORMUsers if c.deprecated is None and orm_group not in c.groups))
